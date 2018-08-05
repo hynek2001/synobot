87d3fcd94f1989c3c00d99edfcff829cd588d5f8
hynek2001 <hynek.los@gmail.com>
2018-08-05 18:33:08 +0200

    doc

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='synobot',
     version='0.1',
     packages=[''],
-    url='https://github.com/hynek2001/',
+    url='https://hynek2001.github.io/synobot/',
     license='MIT',
     author='Hynek',
     author_email='hynek.los@gmail.com',
